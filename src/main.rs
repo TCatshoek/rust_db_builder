34aa387380a6072981357a791a7c7336ecb49e7c
Tom Catshoek <tomcatshoek@zeelandnet.nl>
2022-05-24 18:00:39 +0200

    Yay it works

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,9 @@ use named_tuple::named_tuple;
 use chrono::{Utc, DateTime, ParseResult, ParseError, FixedOffset};
 use std::fs::File;
 use std::thread;
-use polars::df;
-use polars::frame::DataFrame;
-use polars::io::SerReader;
-use polars::prelude::{CsvReader, DataType, Field};
-use polars::series::Series;
-use rusqlite::{params, Connection, Result};
+use csv::Reader;
+use rusqlite::{params, Connection, Result, ToSql};
 use itertools::izip;
-use polars::export::arrow::datatypes::Schema;
 
 /// Build a database
 #[derive(Parser, Debug)]
@@ -36,6 +31,7 @@ struct Args {
 
 struct LogData {
     timestamp: DateTime<Utc>,
+    timestamp_str: String,
     message: String,
     novelty_score: f32
 }
@@ -148,81 +144,31 @@ fn get_hosts_and_services(logpath: &str, noveltypath: &str) -> HashMap<String, H
     return data;
 }
 
-fn strip_column_name_whitespace(df: &mut DataFrame) {
-    let mut new_column_names = Vec::new();
-
-    for mut column_name in df.get_column_names_owned() {
-        column_name = column_name.trim().to_string();
-        // column_name.retain(|c| !c.is_whitespace());
-        new_column_names.push(column_name);
-    }
-
-    df.set_column_names(&new_column_names).unwrap();
-}
-
-fn get_host_service_dataframes(log_path: &PathBuf, novelty_path: &PathBuf) -> (DataFrame, DataFrame) {
-
-    println!("{:?}", log_path);
-    let log_columns: Vec<String> = Vec::from([
-        String::from("datetime"),
-        String::from("message")
-    ]);
-
-    let mut df_log = CsvReader::from_path(log_path)
-        .unwrap()
-        .has_header(true)
-        .infer_schema(Some(100))
-        .with_n_threads(Some(1))
-        .with_parse_dates(true)
-        // .with_columns(log_columns)
-        .finish()
-        .unwrap();
-        // .drop_nulls(None)
-        // .unwrap();
-
-    strip_column_name_whitespace(&mut df_log);
-    println!("{}", df_log.head(Some(5)));
-
-    df_log = df_log.select(log_columns).unwrap();
-
-    let novelty_columns: Vec<String> = Vec::from([
-        String::from("datetime"),
-        String::from("sum scores")
-    ]);
-
-    let mut df_novelty = CsvReader::from_path(novelty_path)
-        .unwrap()
-        .infer_schema(Some(100))
-        .with_delimiter(b';')
-        .has_header(true)
-        .with_parse_dates(true)
-        .with_n_threads(Some(1))
-        // .with_columns(novelty_columns)
-        .finish()
-        .unwrap();
-        // .drop_nulls(None)
-        // .unwrap();
-
-    strip_column_name_whitespace(&mut df_novelty);
-    // println!("{:?}", df_novelty.head(None));
-    df_novelty = df_novelty.select(novelty_columns).unwrap();
-    println!("{}", df_novelty.head(Some(5)));
-    // println!("{:?}", df_novelty.head(Some(5)));
-
-    return (df_log, df_novelty);
-
-}
 
 fn get_relevant_data(log_path: &PathBuf, novelty_path: &PathBuf) -> Vec<LogData> {
     // println!("{:?}, {:?}", log_path, novelty_path);
     let mut log_data = Vec::new();
 
-    let mut log_reader = csv::Reader::from_path(log_path).unwrap();
+    let mut log_reader = csv::ReaderBuilder::new()
+        .from_path(log_path)
+        .unwrap();
     let mut novelty_reader = csv::ReaderBuilder::new()
         .delimiter(b';')
         .flexible(true)
         .from_path(novelty_path)
         .unwrap();
+
+    let log_headers = log_reader.headers().unwrap();
+    let novelty_headers = novelty_reader.headers().unwrap();
+
+    // println!("Log headers: {:?}", log_headers);
+    // println!("Novelty headers: {:?}", novelty_headers);
+
+    let log_datetime_idx = log_headers.iter().position(|r| r == "datetime").unwrap();
+    let log_message_idx = log_headers.iter().position(|r| r == "message").unwrap();
+    let novelty_datetime_idx = novelty_headers.iter().position(|r| r == "datetime").unwrap();
+    let novelty_sum_scores_idx = novelty_headers.iter().position(|r| r == "sum scores")
+        .expect(&*format!("Couldn't find sum scores header in {:?}", novelty_path));
 
     let mut counter = 1;
     for (log, novelty) in zip(log_reader.records(), novelty_reader.records()) {
@@ -231,20 +177,33 @@ fn get_relevant_data(log_path: &PathBuf, novelty_path: &PathBuf) -> Vec<LogData>
 
         // println!("datetime? {}", log_record.get(1).unwrap());
 
-        let cur_datetime = DateTime::parse_from_rfc3339(
-            log_record.get(1).unwrap()
+        let cur_log_datetime = DateTime::parse_from_rfc3339(
+            log_record.get(log_datetime_idx).unwrap()
         );
-        let cur_datetime = match cur_datetime {
+        let cur_log_datetime = match cur_log_datetime {
             Ok(datetime) => datetime,
             Err(error) => {
-                println!("Warning: Couldn't parse datetime {}", log_record.get(1).unwrap());
+                println!("Warning: Couldn't parse log datetime {}", log_record.get(1).unwrap());
                 continue;
             }
         };
 
+        let cur_novelty_datetime = DateTime::parse_from_rfc3339(
+            log_record.get(log_datetime_idx).unwrap()
+        );
+        let cur_novelty_datetime = match cur_novelty_datetime {
+            Ok(datetime) => datetime,
+            Err(error) => {
+                println!("Warning: Couldn't parse novelty datetime {}", log_record.get(1).unwrap());
+                continue;
+            }
+        };
+
+        assert_eq!(cur_novelty_datetime, cur_log_datetime);
+
         // println!("log {:?}", cur_datetime);
 
-        let cur_msg = match log_record.get(5) {
+        let cur_msg = match log_record.get(log_message_idx) {
             None => {
                 println!("Couldn't parse message: {:?}", log_record);
                 continue;
@@ -252,7 +211,7 @@ fn get_relevant_data(log_path: &PathBuf, novelty_path: &PathBuf) -> Vec<LogData>
             Some(x) => String::from(x)
         };
 
-        let cur_novelty_score: Result<f32, _> = match novelty_record.get(1) {
+        let cur_novelty_score: Result<f32, _> = match novelty_record.get(novelty_sum_scores_idx) {
             None => { println!("Couldn't parse novelty score : {:?}", novelty_record); continue; },
             Some(x) => {
                 let tmp: String = x.chars().filter(|c| !c.is_whitespace()).collect();
@@ -265,7 +224,8 @@ fn get_relevant_data(log_path: &PathBuf, novelty_path: &PathBuf) -> Vec<LogData>
         };
 
         let cur_logdata = LogData {
-            timestamp: cur_datetime.with_timezone(&Utc),
+            timestamp: cur_log_datetime.with_timezone(&Utc),
+            timestamp_str: cur_log_datetime.to_string(),
             message: cur_msg,
             novelty_score: cur_novelty_score
         };
@@ -283,7 +243,7 @@ fn main() {
     let args = Args::parse();
 
     // Database
-    let conn = Connection::open("test.db").unwrap();
+    let mut conn = Connection::open("test.db").unwrap();
 
     conn.execute(
         "DROP TABLE IF EXISTS logline", []
@@ -299,6 +259,14 @@ fn main() {
         []
     ).unwrap();
 
+    conn.execute_batch(
+        "PRAGMA journal_mode = OFF;
+             PRAGMA synchronous = 0;
+             PRAGMA cache_size = 1000000;
+             PRAGMA locking_mode = EXCLUSIVE;
+             PRAGMA temp_store = MEMORY;")
+    .unwrap();
+
     println!("Getting hosts and services...");
     let data = get_hosts_and_services(&args.logpath, &args.noveltypath);
 
@@ -306,7 +274,7 @@ fn main() {
     let mut n_jobs = 0;
 
     // Set up reader workers
-    let n_reader_workers = 1;
+    let n_reader_workers = 4;
     let reader_pool = ThreadPool::new(n_reader_workers);
     let (reader_tx, reader_rx) = sync_channel(0);
 
@@ -316,7 +284,7 @@ fn main() {
             let tx = reader_tx.clone();
             n_jobs += 1;
             reader_pool.execute(move|| {
-                let result = get_host_service_dataframes(
+                let result = get_relevant_data(
                     paths.log_path(),
                     paths.novelty_path()
                 );
@@ -325,90 +293,58 @@ fn main() {
         }
     }
 
-    // Set up joining workers
-    let n_joiner_workers = 1;
-    let joiner_pool = ThreadPool::new(n_joiner_workers);
-    let (joiner_tx, joiner_rx) = sync_channel(0);
+    let mut jobs_finished = 0;
 
-    let mut reader_jobs_finished = 0;
-    let reader_recv_thread = thread::spawn(move || {
-        while reader_jobs_finished != n_jobs {
-            let (df_log, df_novelty) = reader_rx.recv().unwrap();
-            reader_jobs_finished += 1;
+    let transaction = conn.transaction().unwrap();
 
-            let tx = joiner_tx.clone();
-            joiner_pool.execute(move|| {
-                // println!("log: \n{}", df_log.head(Some(5)));
-                // println!("novelty: \n{}", df_novelty.head(Some(5)));
-                let df_joined = df_log.left_join(&df_novelty, ["datetime"], ["datetime"]).unwrap();
-                tx.send(df_joined).unwrap();
-            });
-        }
-    });
+    let chunksize = 50;
 
+    {
+        let tx = &transaction;
 
+        let mut insert_query_single = tx.prepare_cached("INSERT INTO logline (datetime, message, novelty_score) VALUES (?1, ?2, ?3)").unwrap();
+        let mut query_params = " (?, ?, ?),".repeat(chunksize);
+        query_params.pop();
+        let query_str= format!("INSERT INTO logline (datetime, message, novelty_score) VALUES {}", query_params);
+        let mut insert_query = tx.prepare_cached(query_str.as_str()).unwrap();
 
-    let mut joiner_jobs_finished = 0;
-    let joiner_recv_thread = thread::spawn(move || {
-        while joiner_jobs_finished != n_jobs {
+        while jobs_finished != n_jobs {
+            let cur_data = reader_rx.recv().unwrap();
+            let cur_data_len = cur_data.len();
+            let n_chunks = cur_data.len() / chunksize;
+            let n_rest = cur_data.len() - (n_chunks * chunksize);
 
-            let joined_df = joiner_rx.recv().unwrap();
+            // Insert data in chunks of chunksize
+            for i in 0..n_chunks {
+                let mut sql_params: Vec<_> = Vec::new();
 
+                for data in &cur_data[i*chunksize..(i+1)*chunksize] {
+                    sql_params.push(&data.timestamp_str as &dyn ToSql);
+                    sql_params.push(&data.message as &dyn ToSql);
+                    sql_params.push(&data.novelty_score as &dyn ToSql);
+                }
 
-
-            // println!("{:?}", joined_df.head(None));
-
-            joiner_jobs_finished += 1;
-
-            let sumscores_column = joined_df.column("sum scores").unwrap().f64();
-            let sumscores_column = match sumscores_column {
-                Ok(x) => x,
-                Err(_) => continue
-            };
-
-            let zipped_columns = izip!(
-                joined_df.column("datetime").unwrap().datetime().unwrap().into_iter(),
-                joined_df.column("message").unwrap().utf8().unwrap().into_iter(),
-                // sumscores_column.into_iter()
-            );
-
-            let mut lines = 0;
-            for (datetime, message) in zipped_columns {
-                lines += 1;
-                println!("lines: {}", lines);
-
-                let d = match datetime {
-                    Some(x) => x,
-                    None => continue
-                };
-
-                let m = match message {
-                    Some(x) => x,
-                    None => continue
-                };
-
-                // let n = match novelty_score {
-                //     Some(x) => x,
-                //     None => continue
-                // };
-                // println!("{} {} {}", d, m, n);
-                conn.execute(
-                    "INSERT INTO logline (datetime, message, novelty_score) VALUES (?1, ?2, ?3)",
-                    params![d, m, 1]
-                ).unwrap();
+                insert_query.execute(&*sql_params).unwrap();
             }
 
+            // Insert remaining data, if any
+            if n_rest > 0 {
 
+                for data in &cur_data[n_chunks*chunksize..] {
+                    insert_query_single.execute(params![data.timestamp_str, data.message, data.novelty_score]).unwrap();
+                }
+            }
 
-
-
-
-            println!("{}/{} shape: {:?}", joiner_jobs_finished, n_jobs, joined_df.shape());
-            // println!("{:?}", joined_df.shape());
+            jobs_finished += 1;
+            println!("[{}/{}] {}", jobs_finished, n_jobs, cur_data.len());
         }
-    });
+    }
+    transaction.commit().unwrap();
 
-    reader_recv_thread.join().unwrap();
-    joiner_recv_thread.join().unwrap();
+    conn.close().unwrap();
+
+    reader_pool.join();
+
+    println!("Done");
 
 }
